0fb07d52ec2a268b7d87b2fd37a7b5873bfef934
nullon <nullon@yahoo.co.jp>
2018-03-14 11:23:24 +0900

    GETによりページを切り替える

---
+++ switch_page.php
@@ -0,0 +1,42 @@
+<?php
+
+page();
+
+exit;
+
+function page(){
+    $pages = [
+        "index",
+        "about",
+        "link",
+    ];
+
+    if(in_array($_GET["p"],$pages)){
+        $exec = "page_".$_GET["p"];
+        $exec();
+    }
+}
+
+function page_index(){
+?>
+    <h1>Title</h1>
+    <p>This is INDEX.</p>
+<?php
+}
+
+function page_about(){
+?>
+    <h2>About<h2>
+<?php
+}
+
+function page_link(){
+?>
+    <h2>Link</h2>
+    <ul>
+        <li><a href="?p=index">INDEX</a></li>
+        <li><a href="?p=about">ABOUT</a></li>
+        <li><a href="?p=link">LINK</a></li>
+    </ul>
+<?php
+}
